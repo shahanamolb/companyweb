38bbd03b917f3821218cf29fc514564c158f1031
shahanamolb <shahanashahu309@gmail.com>
2022-10-15 17:21:42 +0530

    added company website(gadgeon)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoute:Routes=
+[
+  {
+  path:'',
+  component: HomeComponent
+},
+ {
+  path:'home',
+  component: HomeComponent
+ }, 
+ {
+ path:'about',
+ component:AboutComponent
+ },
+{
+path:'services',
+component:ServicesComponent
+},
+{
+  path:'contact',
+  component: ContactComponent
+}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
